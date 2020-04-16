33acfd73a799de878d68a5e98cc2c788c6a2d8fe
masterUNG <phrombutr@gmail.com>
2020-04-16 09:20:11 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:ungsenete/screens/my_service.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: MyService(),
+    );
+  }
+}
